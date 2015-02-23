3d8cfe2f8c4c773c2ed198ca1f36f5e7202e42b3
Ivan Vanderbyl <ivanderbyl@digitalocean.com>
2015-02-23 10:53:46 -0500

    Expose popup service

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import Authentiq from './lib/authentiq';
+import PopupService from './lib/authentiq/services/popup';
 export default Authentiq;
+
+Authentiq.PopupService = PopupService;
+
+window.Authentiq = Authentiq;
